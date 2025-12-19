c0e6d0ccfbbc1d601d93356f231ab89be334d906
alancuevat <alancuevat@gmail.com>
2025-12-19 18:21:00 -0500

    cambioo el orden, productos, despues identidad claurea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import BrandIdentity from "@/components/section/inicio/BrandIdentity";
 import HeroSection from "@/components/section/inicio/herosection";
 import ProductSection from "@/components/section/inicio/productosSection";
-import QualityAndCommitment from "@/components/section/inicio/QualityAndCommitment";
+// import QualityAndCommitment from "@/components/section/inicio/QualityAndCommitment";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
         <HeroSection/>
-        <BrandIdentity/>
         <ProductSection/>
+        <BrandIdentity/>
         {/* <QualityAndCommitment/> */}
     </div>
   );
